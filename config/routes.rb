07eaddc9a37acccf1a383968221e389d4cb8ff8d
Teyoung Hong <teyon2323@gmail.com>
2019-06-21 14:45:59 +0000

    [add&update] 管理者ログイン画面 | deviseコントローラー作成 | kannrisya

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admins, :controllers => {
+    :sessions =>'admins/sessions'
+  }
+
+  devise_for :customers, :controllers => {
+    :registrations => 'customers/registrations',
+    :sessions => 'customers/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'albums#index'
