a25bb00ef09899bd2235447547c0fb946116edd9
takuro0987 <47883574+takuro0987@users.noreply.github.com>
2019-07-30 16:13:44 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
 # 管理者
 
   namespace :admins do
-    resources :customers, only: [:index, :show, :edit]
+    resources :customers, only: [:index, :show, :edit, :update]
     post 'customers/:id/delete' => 'customers#delete'
   end
 
