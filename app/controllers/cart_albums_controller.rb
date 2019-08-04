1639fa554335976a2015cfe173d90a99c9835368
takuro0987 <dfre0821@gmail.com>
2019-08-04 12:07:11 +0000

    [add] cartdeleteボタン

--- app/controllers/cart_albums_controller.rb
+++ app/controllers/cart_albums_controller.rb
@@ -19,16 +19,17 @@ class CartAlbumsController < ApplicationController
         #redirect_to customer_path(current_customer.id)
     end
 
-    def destroy
+    def destroy  
         album = Album.find(params[:album_id])
         cart_album = current_customer.cart_albums.find_by(album_id: album.id)
         cart_album.destroy
         redirect_to customer_path(current_customer.id)
-　  end
-
+    end
+       
     private
-
     def cart_album_params
       params.require(:cart_album).permit(:id, :order_quantity, :customer_id, :album_id)
     end
-end
+  end
+
+
